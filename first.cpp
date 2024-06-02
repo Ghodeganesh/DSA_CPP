f66f682aa1892ba7b22cdd6ab64891cda600a08e
ganesh ghode <ganeshghode2002@gmail.com>
2024-06-02 13:31:51 +0530

    code

--- first.cpp
+++ first.cpp
@@ -1,11 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    // cout << "Namaste Duniya"<<endl;
+    // int val=30;
+    // cout<<"the val is : "<<val<<endl;
 
-    int age=20;
-    cout<<"my age is : "<<age<<endl;
+    // int marks;
+    // cout<<"enter Marks: "<<endl;
+    // cin>>marks;
+    // if(marks>20){
+    //     cout<<"Hello How Are You"<<endl;
+    // }
+    // else{
+    //     cout<<"your Marks are leess"<<endl;
+    // }
 
+    // switch(marks){
+    //     case 10:cout<<"Your Marks are: "<<marks<<endl;
+    //     break;
+    //      case 11:cout<<"Your Marks are: "<<marks<<endl;
+    //     break;
+    //      case 12:cout<<"Your Marks are: "<<marks<<endl;
+    //     break;
+    //      case 13:cout<<"Your Marks are: "<<marks<<endl;
+    //     break;
+    //     default:cout<<"your marks are not defined"<<endl;
 
-    return 0;
+    // }
+
+    // for(;;){
+    //     cout<<"Hello:"<<endl;
+    // }
+
+    // for (char ch = 'a'; ch <= 'z'; ch++)
+    //     ;
+    // {
+    //     cout << "chhar is at : " << ch << endl;
+    // }
+    //   int i;
+    // if(cin<<i){
+    //     cout<<"hello: "<<i;
+    // }
+    if (cout << "Hello ")
+    {
+        cout << "hello: ";
+    }
 }
