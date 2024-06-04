222295b4b93a231fb5b8b6e02ade43482487d91e
ganesh ghode <ganeshghode2002@gmail.com>
2024-06-04 19:34:00 +0530

    pattern

---
+++ Pattern.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // program for square starts
+    // int n;
+    // cout << "Enter The Values of N:" << endl;
+    // cin >> n;
+    // for (int row = 1; row <= n; row++)
+    // {
+    //     for (int col = 1; col <= n; col++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    // program for rectangle
+    // int rowN, colN;
+    // cout << "Enter The Values of Row and Col:" << endl;
+    // cin >> rowN;
+    // cin >> colN;
+    // for (int row = 1; row <= rowN; row++)
+    // {
+    //     for (int col = 1; col <= colN; col++)
+    //     {
+    //         cout << " * ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // program for hollow square
+
+    int row, col;
+    cout << "Enter the Total row : ";
+    cin >> row;
+    cout << "Enter the Total col : ";
+    cin >> col;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            if (i == 0 || i == row - 1)
+            {
+                cout << " * ";
+            }
+            else
+            {
+                if(j == 0 || j == col-1)
+                cout << " * ";
+                else
+                cout<<"   ";
+            }
+        }
+        cout << endl;
+    }
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,21 @@
-  int i;
-    // if(cin<<i){
-    //     cout<<"hello: "<<i;
-    // }
+  int totrow, totcol;
+    cout << "Enter The Value of Row : ";
+    cin >> totrow;
+    cout << "Enter The Value of col: ";
+    cin >> totcol;
+
+    for (int i = 0; i < totrow; i++)
+    {
+        for (int j = 0; j < totcol; j++)
+        {
+            if (i == 0 || i == totrow || j == 0 || j == totcol)
+            {
+                cout << " * ";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
