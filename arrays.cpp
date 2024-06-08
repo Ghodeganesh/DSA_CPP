627d37c6f4a573b6eefd01f499778fb90a376cb4
ganesh ghode <ganeshghode2002@gmail.com>
2024-06-08 17:52:39 +0530

    second Largest Element

--- arrays.cpp
+++ arrays.cpp
@@ -3,15 +3,38 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello"<<endl;
-    int arr[7] = {1, 2, 3, 4, 4, 5, 6};
-    int i=0;int j=6;
-    while(i<j){
-        swap(arr[i],arr[j]);
-        i++;
-        j--;
-    }
-    for(int k=0;k<7;k++){
-        cout<<arr[k]<<" ";
-    }
+    // cout << "Hello"<<endl;
+    // int arr[7] = {1, 2, 3, 4, 4, 5, 6};
+    // int i=0;int j=6;
+    // while(i<j){
+    //     swap(arr[i],arr[j]);
+    //     i++;
+    //     j--;
+    // }
+    // for(int k=0;k<7;k++){
+    //     cout<<arr[k]<<" ";
+    // }
+
+    //Second Largest Element
+    //  int ans=INT8_MIN;
+    //  int secondMx=INT8_MIN;
+    // int arr[9]={3,5,1,78,85,94,6,9};
+    // for(int i=0;i<8;i++){
+    //     if(arr[i]>ans){
+    //         ans=arr[i];
+    //     }
+        
+    // }
+    // for(int j=0;j<8;j++){
+    //     if(arr[j]==ans){
+    //         continue;
+    //     }
+    //     else{
+    //         if(arr[j]>secondMx){
+    //             secondMx=arr[j];
+    //         }
+    //     }
+    // }
+    // cout<<"The Maximum Element : "<<ans<<endl;
+    // cout<<"The Second Maximum Element : "<<secondMx;
 }
