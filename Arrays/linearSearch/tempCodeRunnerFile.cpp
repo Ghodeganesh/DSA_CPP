2f588c1455e7420aef4f7b46b5cb70ee307446da
ganesh ghode <ganeshghode2002@gmail.com>
2024-06-15 13:12:35 +0530

    modified

--- Arrays/linearSearch/tempCodeRunnerFile.cpp
+++ Arrays/linearSearch/tempCodeRunnerFile.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+using namespace std;
+
 int main(){
     // cout<<"Hello";
     int j=-1;
@@ -6,7 +9,7 @@ int main(){
 
     for(int i=0;i<n;i++){
         if(arr[i]==0){
-            j=1;
+            j=i;
             break;
         }
     }
@@ -21,4 +24,5 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+    return 1;
 }
