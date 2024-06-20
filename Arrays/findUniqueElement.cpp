b78156830bf2e870747b44a110128f1cba6274b0
ganesh ghode <ganeshghode2002@gmail.com>
2024-06-20 12:08:01 +0530

    twoSumProblem

--- Arrays/findUniqueElement.cpp
+++ Arrays/findUniqueElement.cpp
@@ -1,24 +1,56 @@
 #include <iostream>
 using namespace std;
 
-int findUniqueElement(int arr[], int size)
+// int findUniqueElement(int arr[], int size)
+// {
+//     int ans = 0;
+//     for (int i = 0; i < size; i++)
+//     {
+//         ans = ans ^ arr[i];
+//     }
+//   return ans;
+// }
+
+bool twoSum(int arr[], int size, int target)
 {
-    int ans = 0;
     for (int i = 0; i < size; i++)
     {
-        ans = ans ^ arr[i];
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
+                cout << "the Ith index at " << i << endl;
+                cout << "the value  at i: " << arr[i] << "," << endl;
+
+                cout << "the Jth index at " << j << endl;
+                cout << "the value at j: " << arr[j] << endl;
+
+                return true;
+            }
+        }
     }
-  return ans;
+    return false;
 }
 
 int main()
 {
 
     // int arr[] = {10, 20, 30, 20, 30};
-    int arr[]={1,2,3,3,1};
+    int arr[] = {1, 2, 3, 3, 1};
     int size = 5;
-    int final = findUniqueElement(arr, size);
-    cout << "the Unique Element : " << final;
+    int target = 42;
+
+    bool val = twoSum(arr, size, target);
+    if (val == true)
+    {
+        cout << "the Sum Found";
+    }
+    else
+    {
+        cout << "the Sum Not found";
+    }
+    // int final = findUniqueElement(arr, size);
+    // cout << "the Unique Element : " << final;
 
     return 0;
 }
