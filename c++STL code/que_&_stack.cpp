7445fa67a908265ae875d0a7cdb70581a7b445e4
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-07-01 13:45:38 +0530

    que & stack

---
+++ c++STL code/que_&_stack.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    // queue<int> arr2;
+
+    // arr.push(10);
+    // arr.push(20);
+
+    // // cout<<"size:"<<arr.size();
+    // // cout<<"front ele: "<<arr.front()<<endl;
+    // // cout<<"rear ele: "<<arr.back()<<endl;
+
+    // arr.pop();
+    // //looping is not possible in queue  :
+
+    // // for (int it : arr2)
+    // // {
+    // //     cout << it << " ";
+    // // }
+
+    // STACK :
+
+    stack<int>arr2;
+
+    arr2.push(10);
+    arr2.push(20);
+    arr2.push(30);
+    arr2.push(40);
+ 
+
+ cout<<arr2.size()<<endl;
+ cout<<arr2.top()<<endl;
+ 
+
+    
+}
