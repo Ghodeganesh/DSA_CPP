d6b3b025b8423a11d8099d4ecb77d56ec182223e
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-07-01 17:45:12 +0530

    algorithms in c++

---
+++ c++STL code/algorithms.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int main()
+{
+
+    // vector<int> arr;
+
+    // arr.push_back(10);
+    // arr.push_back(10);
+    // arr.push_back(1);
+    // arr.push_back(3);
+    // arr.push_back(7);
+
+    // iota(arr.begin(),arr.end(),100);
+
+    // for (int it : arr)
+    // {
+    //     cout << it << " ";
+    // }
+
+    // for (int it : arr)
+    // {
+    //     cout << it << " ";
+    // }
+    // cout << endl;
+
+    // vector<int> result(arr.size());
+
+    // partial_sum(arr.begin(), arr.end(), result.begin());
+
+    // for (int i : result)
+    // {
+    //     cout << i << " ";
+    // }
+
+    // vector<int> arr2;
+
+    // arr2.push_back(19);
+    // arr2.push_back(12);
+
+    // arr2.push_back(10);
+    // arr2.push_back(14);
+    // arr2.push_back(1);
+    // arr2.push_back(3);
+    // arr2.push_back(7);
+
+    // for (int it : arr2)
+    // {
+    //     cout << it << " ";
+    // }
+    // cout << endl;
+    //   cout<<arr[2];
+    // cout<<"Before Sorting: "<<endl;
+    // for (int it : arr)
+    // {
+    //     cout << it << " ";
+    // }
+    // cout<<endl;
+    // sort(arr.begin(),arr.end());
+
+    //  cout<<"After Sorting: "<<endl;
+    // for (int it : arr)
+    // {
+    //     cout << it << " ";
+    // }
+
+    // int ans = accumulate(arr.begin(), arr.end(), 100);
+    // cout<<ans;
+
+    // int ans = inner_product(arr.begin(), arr.end(), arr2.begin(), 0);
+    // cout <<"Total Sum: "<< ans;
+
+    vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(11);
+    arr.push_back(13);
+    arr.push_back(21);
+    arr.push_back(12);
+    int target = 12;
+    int ans = binary_search(arr.begin(), arr.end(), target);
+    cout<<"final ans: "<<ans;
+}
