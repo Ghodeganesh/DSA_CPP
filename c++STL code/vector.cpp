5b71ca5bcace048df1dd09b3cec2e2de8773785d
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-07-01 11:02:09 +0530

    stl in c++

---
+++ c++STL code/vector.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cout << "Stl In C++" << endl;
+
+    vector<int> arr(5);
+
+    cout<<"size: "<< arr.size()<<endl;
+
+    cout<< arr.begin<<endl;
+
+
+    // arr.push_back(1);
+    // arr.push_back(10);
+    // cout << arr[0] << " ";
+    // cout << arr[1]<<endl;
+    // cout << arr[3]<<endl;
+
+    // cout<<arr.end();
+
+    // cout<<arr.front()<<" ";
+    // cout<<arr.back()<<" ";
+    // cout<<arr.pop_back();
+    // cout<<arr.size();
+    // arr.insert(arr.begin(),100);
+    // cout<<endl;
+    // cout<<arr[0];
+    // cout<<arr.begin();
+    
+}
