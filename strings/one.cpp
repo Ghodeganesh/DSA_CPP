5756ce0a3dd3a26a286d49e7257ae814a74807d9
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-07-17 00:43:11 +0530

    string

---
+++ strings/one.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // string name;
+    // cout<<"enter the name: ";
+    // cin>>name;
+    // cout<<"the name is : "<<name;
+
+    string sentance;
+    cout<<"enter data: ";
+   getline(cin,sentance,'\n');
+   cout<<sentance;
+}
