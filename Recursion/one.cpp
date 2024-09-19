b3c5d5d0023d87f26299082b148f6bdb14856f4f
Ganesh Ghode <ganeshghode2002@gmail.com>
2024-09-19 14:19:23 +0530

    recursion

--- Recursion/one.cpp
+++ Recursion/one.cpp
@@ -16,20 +16,40 @@ using namespace std;
 //         return checkPal(str, start + 1, end - 1);
 // }
 
-int count(string str, int index)
+// int count(string str, int index)
+// {
+//     if (index == -1)
+//     {
+//         return 0;
+//     }
+//     if (str[index] == 'a' || str[index] == 'e' || str[index] == 'i' || str[index] == 'o' || str[index] == 'u')
+//     {
+//         return 1 + count(str, index - 1);
+//     }
+//     else
+//     {
+//         return count(str, index - 1);
+//     }
+// }
+
+// void rev(string &str, int start, int end)
+// {
+//     if (start >= end)
+//     {
+//         return;
+//     }
+//     char c = str[start];
+//     str[start] = str[end];
+//     str[end] = c;
+
+//     rev(str,start+1,end-1);
+// }
+void lowToUpperCase(string &str, int index)
 {
     if (index == -1)
-    {
-        return 0;
-    }
-    if (str[index] == 'a' || str[index] == 'e' || str[index] == 'i' || str[index] == 'o' || str[index] == 'u')
-    {
-        return 1 + count(str, index - 1);
-    }
-    else
-    {
-        return count(str, index - 1);
-    }
+        return;
+    str[index] = 'A' + str[index] - 'a';
+    lowToUpperCase(str, index - 1);
 }
 
 int main()
@@ -39,5 +59,11 @@ int main()
     cin >> name;
 
     // cout << checkPal(name, 0, 4);
-     cout << count(name,4);
+    //  cout << count(name,4);
+
+    // rev(name, 0, 5);
+    // cout << name;
+
+    lowToUpperCase(name, 5);
+    cout << name;
 }
